67da4ad8097b78f99ef59fa3445ef5d3854d7482
andrzejwl <andrzejw239@gmail.com>
2021-10-17 23:30:43 +0200

    fix: node dependencies

--- docker-compose.py
+++ docker-compose.py
@@ -2,9 +2,10 @@
 generate docker-compose dynamically
 """
 
-from typing import Dict, List
+from typing import List
 import yaml
-import codecs
+
+yaml.Dumper.ignore_aliases = lambda *args : True # for some reason pyyaml is dumping pointer references instead of arrays without this line
 
 class Compose(object):
     def __init__(self, version='3.9') -> None:
@@ -40,7 +41,6 @@ class Compose(object):
         # remove single quotes from entrypoint
         f = open(filename, 'r+')
         contents = f.read()
-        print(contents)
         contents = contents.replace("'[", "[")
         contents = contents.replace("]'", "]")
         f.seek(0)
@@ -53,13 +53,19 @@ compose = Compose()
 compose.add_service(name='node-discovery', image='evoting_node-discovery', ports=['9999:9999',])
 compose.add_service(name='connector', image='evoting_connector', ports=['1234:1234',], env_vars=['ND_ADDR=node-discovery:9999'])
 
+dependencies = ['node-discovery']
+
 # blockchain nodes
 for i in range(10):
+    deps = dependencies[:]
+    if i != 0:
+        deps.append(f'node-{i}')
+
     compose.add_service(
         name=f'node-{i+1}', 
         image='evoting_node', 
         ports=[f'{1337+i}:{1337+i}',], 
-        depends_on=['node-discovery'], 
+        depends_on=deps, 
         env_vars=[
             f'HOSTNAME=node-{i+1}', 
             'DISCOVERY_ADDR=node-discovery:9999',
